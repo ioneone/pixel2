8734b97a039e4c2014c74ea9b921ae85ddee5b24
ioneone <ionejunhong@gmail.com>
2022-05-13 22:03:22 -0700

    add starter code

---
+++ html/src/wang/junhong/pixel2/client/HtmlLauncher.java
@@ -0,0 +1,23 @@
+package wang.junhong.pixel2.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+import wang.junhong.pixel2.Pixel2;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Pixel2();
+        }
+}
